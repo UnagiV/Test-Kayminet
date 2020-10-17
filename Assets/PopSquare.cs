1320bad72b6900f667188f392164aaa5791e0e5c
yann grard <yanngrard@gmail.com>
2020-10-17 16:40:19 +0200

    add square code

---
+++ Assets/PopSquare.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopSquare : MonoBehaviour
+{
+
+    public GameObject RightSqare;
+    public GameObject LeftSquare;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            LeftSquare.SetActive(true);
+            RightSqare.SetActive(false);
+
+            //do some code for sound effect
+        }
+
+        if (Input.GetKey("right"))
+        {
+            LeftSquare.SetActive(false);
+            RightSqare.SetActive(true);
+
+            //do some code for sound effect
+        }
+    }
+}
